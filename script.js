634e686cff1701a928da116d3914a5f70e68d9de
ClarkeTj <clarketejiri@gmail.com>
2025-07-21 00:53:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('get-joke').addEventListener('click', async () => {
+  const jokeEl = document.getElementById('joke');
+  jokeEl.textContent = 'Loading joke...';
+
+  try {
+    const res = await fetch('https://v2.jokeapi.dev/joke/Programming?type=single');
+    const data = await res.json();
+    jokeEl.textContent = data.joke || "Couldn't fetch a joke!";
+  } catch (err) {
+    jokeEl.textContent = 'Error loading joke!';
+    console.error(err);
+  }
+});
